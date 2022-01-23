4175c2588e56392bdb16035615c73c3163a5587c
Jana Semenova <jana.semenova@yolawo.de>
2022-01-23 20:15:15 +0100

    removed comments

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,6 @@ import { Menu } from "./components/Menu";
 
 async function run() {
   const app = new PIXI.Application({
-    //resolution: devicePixelRatio || 1,
     backgroundColor: 0x202470,
   });
 
@@ -19,27 +18,16 @@ async function run() {
 
   document.body.appendChild(app.view);
 
-  // Init game controller and initial scene.
   const main = new Main(app);
   main.setScene(new Menu(main));
 
-  // Resize logic for running full screen with minimum dimenions.
   const resize = () => {
     const targetSize = { width: 400, height: 600 };
     const windowSize = { width: window.innerWidth, height: window.innerHeight };
-    // Calculate size for renderer.
     const viewSize = main.calculateViewSize(targetSize, windowSize);
-
-    // Resize Pixi renderer.
     app.renderer.resize(viewSize.width, viewSize.height);
-
-    // Adjust canvas to fill screen.
     app.view.style.width = `100%`;
     app.view.style.height = `90%`;
-    // app.view.style.width = `${windowSize.width}px`;
-    // app.view.style.height = `${windowSize.height}px`;
-
-    // Alert the game controller of the new size, to pass along to the current scene.
     main.onResize(viewSize.width, viewSize.height);
   };
 
